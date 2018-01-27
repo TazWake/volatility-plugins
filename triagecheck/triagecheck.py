5ac3fc955032f1711dfe4e33efb648b43d2be6ec
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-01-27 20:01:37 +0000

    Update triagecheck.py

--- triagecheck/triagecheck.py
+++ triagecheck/triagecheck.py
@@ -1,17 +1,17 @@
 # Triage Check
-#
-# This plugin looks at some obvious signs of malicious activity and tries to highlight them.  
-# 
+
+# This plugin highlights obvious signs of malicious activity.
+
 # #############################
 # #### !!! LIMITATIONS !!! ####
 # #############################
 #
-# This is not an indepth tool that checks for hollowing, injection etc. 
+# This is not an indepth tool that checks for hollowing, injection etc.
 # It makes lots of assumptions. It uses pslist.
-# Malware can evade this scan. This checks for LOW HANGING FRUIT. 
-# Do not think a clean scan with this plugin means the 
+# Malware can evade this scan. This checks for LOW HANGING FRUIT.
+# Do not think a clean scan with this plugin means the
 # system is free from tampering.
-# This plugin WILL, however, find some common characteristics 
+# This plugin WILL, however, find some common characteristics
 # of attacker behaviour. Use it as PART of your arsenal.
 #
 # ############################
@@ -44,6 +44,7 @@ import volatility.plugins.common as common
 
 from volatility.renderers import TreeGrid
 
+
 class TriageCheck(common.AbstractWindowsCommand):
     '''Checks for OBVIOUS signs of tampering'''
 
@@ -61,8 +62,8 @@ class TriageCheck(common.AbstractWindowsCommand):
             holder = ""
             procname = str(task.ImageFileName)
             pid = int(task.UniqueProcessId)
-            
-            # Check smss for known attacks 
+
+            # Check smss for known attacks
             # Should only be 1 instance of smss, running from system32
             check = "smss.exe"
             if procname == check:
@@ -75,8 +76,8 @@ class TriageCheck(common.AbstractWindowsCommand):
                 imgpath = str(task.Peb.ProcessParameters.ImagePathName)
                 path = str("\system32\smss.exe")
                 if path in imgpath.lower():
-                   # valid path
-                   holder = "bypass"
+                    # valid path
+                    holder = "bypass"
                 else:
         	    # invalid path
                     response = "SMSS launched from invalid path"
@@ -91,9 +92,9 @@ class TriageCheck(common.AbstractWindowsCommand):
                    # valid path
                    holder = "bypass"
                 else:
-        	    # invalid path
-                    response = "CSRSS launched from invalid path"         
-           
+                # invalid path
+                    response = "CSRSS launched from invalid path"
+
             # Check services.exe is running from system32
             check = "services.exe"
             if procname.lower() == check:
@@ -105,7 +106,7 @@ class TriageCheck(common.AbstractWindowsCommand):
                 else:
                     # invalid path
                     response = "Services.exe running from unusual location"
-                    
+
             # Check for impersonation
             # Check for CSRSS impersonation
             check = ["cssrss.exe", "cssrs.exe", "csrss.exe"]
@@ -114,21 +115,21 @@ class TriageCheck(common.AbstractWindowsCommand):
                 # looks suspicious
                 response = "Possible impersonation attempt - CSRSS" 
             # Check for SVCHost impersonation
-            check = ["scvhost.exe","svch0st.exe","sscvhost.exe","svcchost.exe","scvh0st.exe","svchozt.exe","svchot.exe","scvhot.exe"]
+            check = ["scvhost.exe", "svch0st.exe", "sscvhost.exe", "svcchost.exe", "scvh0st.exe", "svchozt.exe", "svchot.exe", "scvhot.exe"]
             if procname.lower() == check:
                 # possible impersonation
                 response = "Posible impersonation of SVCHOST.EXE"
             # Check for DLLHOST impersonation
-            check = ["dllh0st.exe","dllhot.exe","d1lhost.exe","dl1host.exe","d11host.exe","d11h0st.exe","dIIhost.exe","dIIh0st.exe","dIlhost.exe","dlIhost.exe"]
+            check = ["dllh0st.exe", "dllhot.exe", "d1lhost.exe", "dl1host.exe", "d11host.exe", "d11h0st.exe", "dIIhost.exe", "dIIh0st.exe", "dIlhost.exe", "dlIhost.exe"]
             if procname.lower() == check:
                 # possible impersonation
                 response = "Posible impersonation of DLLHOST.EXE"
             # Check for LSASS impersonation
-            check = ["lsas.exe","lssas.exe","ls4ss.exe","lsasss.exe","lssass.exe","lsaas.exe"]
+            check = ["lsas.exe", "lssas.exe", "ls4ss.exe", "lsasss.exe", "lssass.exe", "lsaas.exe"]
             if procname.lower() == check:
                 # possible impersonation
                 response = "Posible impersonation of LSASS.EXE"
-                
+
             # Scan for LSASS oddities
             # Check for multiple lsass, eg Stuxnet :-)
             check = "lsass.exe"
@@ -152,7 +153,7 @@ class TriageCheck(common.AbstractWindowsCommand):
             # first, account for system process being the odd one out
             if procname.lower() == "system":
                 holder = "bypass"
-                # Bypass other checks here.                   
+                # Bypass other checks here.
             else:
                 # check for data collection issues where procname doesn't contain full file name
                 if "." not in procname:
@@ -166,22 +167,22 @@ class TriageCheck(common.AbstractWindowsCommand):
                     if extension.lower() != "exe":
                         # possibly suspicious
                         response = "Possibly suspicious extension"
-                    
+
             # output in "Unified Output format"
             if response != "-":
                 yield (0, [
-                          int(pid),
-                          str(procname),
-                          str(response),
-                          ])
+                    int(pid),
+                    str(procname),
+                    str(response),
+                    ])
             else:
                 continue
-                
+
     def unified_output(self,data):
         tree = [
-                ("PID",int),
-                ("Filename",str),
-                ("Triage Response",str),
-               ]
+            ("PID",int),
+            ("Filename",str),
+            ("Triage Response",str),
+            ]
 
         return TreeGrid(tree, self.generator(data))
