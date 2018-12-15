db6ec1aea18420aa16ce1e50582e60f80d006594
Taz Wake <26778008+TazWake@users.noreply.github.com>
2018-12-15 19:07:37 +0000

    Update ramscan.py

--- ramscan/ramscan.py
+++ ramscan/ramscan.py
@@ -6,39 +6,16 @@
 # Ramscan extracts process name, PID and parent PID, and command line.
 # Each process VAD is checked and entries with Read/Write/Execute are marked
 # as suspicious
-#
-# Authors:
-# Taz Wake (t.wake@halkynconsulting.co.uk)
-#
-# This file is part of Volatility.
-#
-# Volatility is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Volatility is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Volatility.  If not, see <http://www.gnu.org/licenses/>.
-
 import volatility.plugins.common as common
 import volatility.utils as utils
 import volatility.win32 as win32
-
 from volatility.renderers import TreeGrid
-
 class RamScan(common.AbstractWindowsCommand):
     '''List processes, PID, PPID, Command Line, VAD Status'''
-
     def calculate(self):
         addr_space = utils.load_as(self._config)
         tasks = win32.tasks.pslist(addr_space)
         return tasks
-
     def generator(self, data):
         cmdline = ""
         vf = ""
@@ -58,7 +35,6 @@ class RamScan(common.AbstractWindowsCommand):
                 str(cmdline),
                 str(vf),
             ])
-
     def unified_output(self, data):
         tree = [
             ("Name", str),
@@ -67,5 +43,4 @@ class RamScan(common.AbstractWindowsCommand):
             ("Command Line", str),
             ("VAD", str),
         ]
-
         return TreeGrid(tree, self.generator(data))
